28de3d99b7a55a881f70faf2286ecd3f9511cd00
TheDarkWolfer <97792990+TheDarkWolfer@users.noreply.github.com>
2022-01-18 07:44:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function calculIMC() {
+    var P = document.getElementById("poids").value;
+    var T = document.getElementById("taille").value;
+    var IMC = P/((T/100)**2)
+    IMC = IMC.toFixed(2)
+    //alert("Valeur de l'IMC : "+IMC);
+    document.getElementById("results").value = IMC;
+}
+
+function main() {
+
+}
