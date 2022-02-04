37ae99627c7785b4b09672d1e2f2d54164bb39d5
TheDarkWolfer <97792990+TheDarkWolfer@users.noreply.github.com>
2022-02-04 17:05:41 +0100

    Delete main.js

--- javascript/main.js
+++
@@ -1,7 +0,0 @@
-function calculIMC() {
-    var P = document.getElementById("poids").value;
-    var T = document.getElementById("taille").value;
-    var IMC = P/((T/100)**2)
-    IMC = IMC.toFixed(2)
-    document.getElementById("results").value = IMC;
-}
